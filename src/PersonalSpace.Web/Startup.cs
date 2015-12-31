150dc0d09c37c49f1c6a937f0b889952d457d6a4
logic01 <abstractlogic01@gmail.com>
2015-12-31 10:44:52 -0700

    updated

--- src/PersonalSpace.Web/Startup.cs
+++ src/PersonalSpace.Web/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
