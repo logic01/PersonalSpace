793c9136f5ea2bd0f597d6956f5e81cbdfca8373
logic01 <abstractlogic01@gmail.com>
2015-12-31 09:50:17 -0700

    apparently i missed files!

---
+++ src/PersonalSpace.Web/wwwroot/js/sidebar.js
@@ -0,0 +1,21 @@
+﻿/* site.js */
+
+(function () {
+
+    var $sidebar = $("#sidebar");
+    var $icon = $("#sidebartoggle i.fa");
+
+    $("#sidebartoggle").on("click", function () {
+
+        $sidebar.toggleClass("hide-sidebar");
+
+        if ($sidebar.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+        }
+    });
+
+})();
